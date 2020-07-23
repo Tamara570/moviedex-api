7d7dd3e0738f01729125efa271a47478ca4cd2ba
Tamara Walker <tamaralw115@gmail.com>
2020-07-22 20:08:56 -0400

    updated authorization

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const moviedex = require('./moviedex.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+  next()
+})
 
 app.get('/movie', function handleGetMovie(req, res) {
   let response = moviedex;
@@ -27,7 +42,7 @@ app.get('/movie', function handleGetMovie(req, res) {
   //filter movies by avg_vote
   if (req.query.avg_vote) {
     response = response.filter(moviedex => 
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+      Number(moviedex.avg_vote) >= Number(req.query.avg_vote)
       )
   }
 
